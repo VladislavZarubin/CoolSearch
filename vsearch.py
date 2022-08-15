21844e7184b35105601499ac4c468758b3d76055
VladislavZarubin <106274858+VladislavZarubin@users.noreply.github.com>
2022-08-15 19:14:24 +0300

    Working app with old codebase

---
+++ vsearch.py
@@ -0,0 +1,3 @@
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """Возвращает множество букв из 'latters', найденых в указанной фразе."""
+    return set(letters).intersection(set(phrase))
